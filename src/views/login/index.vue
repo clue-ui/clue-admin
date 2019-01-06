b3b5d7d005490e7e5f7eb77d3fda921f784f16ea
huangchao <ftchao@local.com.cn>
2019-01-06 18:04:27 +0800

    fix login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,24 +1,46 @@
 <template>
   <div class="app-login">
-    <h1>This is an login page</h1>
-    <c-form class="login-form">
-      <c-form-item>
-        <c-input placeholder="用户名/手机号"></c-input>
-      </c-form-item>
+    <div class="pt-24">
+      <c-section class="login-form">
 
-      <c-form-item>
-        <c-input placeholder="密码"></c-input>
-      </c-form-item>
+        <!-- title -->
+        <h2 class="my-8">{{title}}</h2>
 
-      <c-form-item>
-        <c-button type="info" @click="onLogin">登陆</c-button>
-      </c-form-item>
-    </c-form>
+        <!-- login form -->
+        <c-form>
+          <c-form-item>
+            <c-input placeholder="手机号或账号"></c-input>
+          </c-form-item>
+
+          <c-form-item>
+            <c-input placeholder="输入密码"></c-input>
+          </c-form-item>
+
+          <c-form-item>
+            <div class="text-right">忘记密码</div>
+          </c-form-item>
+
+          <c-form-item>
+            <c-button
+              type="primary"
+              class="w-full"
+              @click="onLogin"
+            >登陆</c-button>
+          </c-form-item>
+        </c-form>
+      </c-section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'app-login',
+  data () {
+    return {
+      title: 'Welcome Clue - Admin'
+    }
+  },
   methods: {
     onLogin () {
       this.$router.push({name: 'home'})
@@ -29,10 +51,15 @@ export default {
 
 <style lang="scss" scoped>
 .app-login {
+  @apply h-screen;
+  @apply text-center;
+  @apply bg-grey;
+
+  // login form
   .login-form {
-    width: 600px;
-    @apply border;
-    @apply m-auto;
+    @apply max-w-sm;
+    @apply bg-white;
+    @apply px-12 py-4 m-auto;
   }
 }
 </style>
