ab1e5989799ec604cc646fbe6fd04dc68e21ab06
huangchao <ftchao@local.com.cn>
2019-03-12 13:44:47 +0800

    fix: [goods] add

--- src/views/goods/src/goods-list.vue
+++ src/views/goods/src/goods-list.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="goods-list clue table">
     <div class="clue table-row" v-for="(v, id) in tableData" :key="id">
-      <div class="clue table-col">{{v.id}}</div>
-      <div class="clue table-col">{{v.title}}</div>
-      <div class="clue table-col">{{v.number}}</div>
+      <div class="clue table-col w-8 text-center">{{v.id}}</div>
+      <div class="goods-info flex-1">
+        <img class="goods-img" :src="v.image">
+        <div class="">
+          <h4 class="goods-title">{{v.title}}</h4>
+
+          <div>{{v.time}}</div>
+        </div>
+      </div>
+      <!-- <div class="clue table-col">{{v.title}}</div> -->
+      <div class="clue table-col">库存: {{v.number}}</div>
       <div class="clue table-col">{{v.time}}</div>
 
       <div class="clue table-col">
-        <c-button class="text-xs">操作</c-button>
+        <!-- <c-button class="text-xs">操作</c-button> -->
       </div>
     </div>
   </div>
@@ -15,6 +23,7 @@
 
 <script>
 import moment from 'moment'
+import 'moment/locale/zh-cn'
 export default {
   name: 'goods-list',
   data () {
@@ -23,12 +32,18 @@ export default {
     }
   },
   mounted () {
+    let imgs = [
+      '/images/shuiguo-201903111737002.jpg',
+      '/images/baicai-201903111726001.jpeg'
+    ]
+
     for (var i = 0; i < 5; i++) {
       this.tableData.push({
         id: i+1,
-        title: 'lskdlksd',
+        title: '希腊大白菜, 又脆又甜',
+        image: imgs[i],
         number: Math.random() * 1000000 & 0xFFFFFFFF,
-        time: moment().format('l')
+        time: moment().format('YYYY/MM/DD H:mm')
       })
     }
   }
--- src/views/goods/src/index.vue
+++ src/views/goods/src/index.vue
@@ -2,9 +2,11 @@
   <div class="clue goods">
     <div>
       <div class="flex">
-        <c-input></c-input>
-        <!-- <c-button>Search</c-button> -->
+        <c-input placeholder="enter search"></c-input>
+        <c-button class="ml-2">搜 索</c-button>
       </div>
+
+      <div></div>
     </div>
 
     <goods-list class="mt-2"></goods-list>
